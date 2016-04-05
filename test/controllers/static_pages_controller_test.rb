6572eeb54f26f7e1122f7e02503aaaad20807d0a
ThomNTH <thomnth1602@gmail.com>
2016-04-05 11:14:12 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -16,5 +16,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+
   end
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end  
 end
